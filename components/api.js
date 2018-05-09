87c7574d35f13795f5c3c5e7021098681ae7c4d4
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-05-09 16:02:20 -0400

    Add mp3 uploading capabilities

--- components/api.js
+++ components/api.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer');
 const mongoose = require('mongoose')
 const Word = require('../models/word');
+const mongodb = require('mongodb')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const { Readable } = require('stream');
+
 let config
 
 try {
@@ -11,16 +17,21 @@ try {
   config = {};
 }
 
-const mongoDB = process.env.DB_URI || config.DB_URI
+const mongoDBURI = process.env.DB_URI || config.DB_URI
 
-// ---------------------------------------------------------------------------
-// Express API
-// ---------------------------------------------------------------------------
-
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDBURI);
 mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const mongooseConnection = mongoose.connection;
+mongooseConnection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+let db;
+MongoClient.connect(process.env.DB_HOST || config.DB_HOST, (err, client) => {
+  if (err) {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+  }
+  db = client.db(process.env.DB_NAME || config.DB_NAME);
+});
 
 module.exports = (io) => {
 
@@ -57,6 +68,69 @@ module.exports = (io) => {
       res.send(words);
     })
   })
+
+  router.post('/upload', (req, res) => {
+    const storage = multer.memoryStorage()
+    const upload = multer({ storage: storage, limits: { fields: 1, fileSize: 6000000, files: 1, parts: 2 }});
+    upload.single('track')(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ message: "Upload Request Validation Failed" });
+      } else if(!req.body.name) {
+        return res.status(400).json({ message: "No track name in request body" });
+      }
+
+      let trackName = req.body.name;
+
+      // Covert buffer to Readable Stream
+      const readableTrackStream = new Readable();
+      readableTrackStream.push(req.file.buffer);
+      readableTrackStream.push(null);
+
+      let bucket = new mongodb.GridFSBucket(db, {
+        bucketName: 'tracks'
+      });
+
+      let uploadStream = bucket.openUploadStream(trackName);
+      let id = uploadStream.id;
+      readableTrackStream.pipe(uploadStream);
+
+      uploadStream.on('error', () => {
+        return res.status(500).json({ message: "Error uploading file" });
+      });
+
+      uploadStream.on('finish', () => {
+        return res.status(201).json({ message: "File uploaded successfully, stored under Mongo ObjectID: " + id });
+      });
+    });
+  });
+
+  router.get('/play/:trackID', (req, res) => {
+    try {
+      var trackID = new ObjectID(req.params.trackID);
+    } catch(err) {
+      return res.status(400).json({ message: "Invalid trackID in URL parameter. Must be a single String of 12 bytes or a string of 24 hex characters" });
+    }
+    res.set('content-type', 'audio/mp3');
+    res.set('accept-ranges', 'bytes');
+
+    let bucket = new mongodb.GridFSBucket(db, {
+      bucketName: 'tracks'
+    });
+
+    let downloadStream = bucket.openDownloadStream(trackID);
+
+    downloadStream.on('data', (chunk) => {
+      res.write(chunk);
+    });
+
+    downloadStream.on('error', () => {
+      res.sendStatus(404);
+    });
+
+    downloadStream.on('end', () => {
+      res.end();
+    });
+  });
 
   return router
 }
