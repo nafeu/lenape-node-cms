0567abfdc80f60db7beb1062db900181f0a96e46
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-05-08 21:59:45 -0400

    Integrate mongoose and first model

--- components/api.js
+++ components/api.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const mongoDB = 'mongodb://127.0.0.1/my_database'
+const mongoose = require('mongoose')
+const Word = require('../models/word');
 
 // ---------------------------------------------------------------------------
 // Express API
 // ---------------------------------------------------------------------------
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = (io) => {
 
@@ -18,6 +26,21 @@ module.exports = (io) => {
 
   router.get('/test', (req, res) => {
     res.status(200).send('OK')
+  })
+
+  router.post('/word', (req, res) => {
+
+    var word = new Word();
+    word.name = req.body.name;
+
+    // save the word and check for errors
+    word.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Word created!' });
+    });
+
   })
 
   return router
---
+++ models/word.js
@@ -0,0 +1,8 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var WordSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Word', WordSchema);
