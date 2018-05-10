2efde1475a319ee613358cc05814bdcf0790acf5
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-05-10 01:40:17 -0400

    Turn whiteboard into chalkboard

--- client/views/record/record.js
+++ client/views/record/record.js
@@ -16,7 +16,10 @@ angular.module('myApp.record', ['ngRoute'])
   $scope.snapshots = [];
 
   $scope.blob;
-  $scope.blobUrl = "";
+  $scope.chalk = {
+    lineWidth: 4,
+    color: "white"
+  };
 
   $scope.createWord = function(){
     apiService.uploadAudio($scope.blob).then(function(res){
@@ -93,28 +96,23 @@ angular.module('myApp.record', ['ngRoute'])
   }
 
   var socket = io();
-  var canvas = document.getElementsByClassName('whiteboard')[0];
+  var canvas = document.getElementsByClassName('chalkboard')[0];
   var colors = document.getElementsByClassName('color');
   var context = canvas.getContext('2d');
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Make our in-memory canvas
   var inMemCanvas = document.createElement('canvas');
   var inMemCtx = inMemCanvas.getContext('2d');
-
   var exportCanvas = document.createElement('canvas');
   var exportCtx = exportCanvas.getContext('2d');
 
-  var current = {
-    color: 'black'
-  };
   var drawing = false;
 
   canvas.addEventListener('mousedown', onMouseDown, false);
   canvas.addEventListener('mouseup', onMouseUp, false);
   canvas.addEventListener('mouseout', onMouseUp, false);
   canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-
-  console.log(canvas.getBoundingClientRect());
 
   for (var i = 0; i < colors.length; i++){
     colors[i].addEventListener('click', onColorUpdate, false);
@@ -128,7 +126,6 @@ angular.module('myApp.record', ['ngRoute'])
     var img=new Image();
     img.src=data;
     setTimeout(function(){
-      console.log("drawing...");
       context.drawImage(img, 0, 0, canvas.width, canvas.height);
     }, 1000);
   })
@@ -136,13 +133,12 @@ angular.module('myApp.record', ['ngRoute'])
   window.addEventListener('resize', onResize, false);
   onResize();
 
-
-  function drawLine(x0, y0, x1, y1, color, emit){
+  function drawLine(x0, y0, x1, y1, color, lineWidth, emit){
+    context.lineWidth = lineWidth;
     context.beginPath();
     context.moveTo(x0, y0);
     context.lineTo(x1, y1);
     context.strokeStyle = color;
-    context.lineWidth = 4;
     context.stroke();
     context.closePath();
 
@@ -155,6 +151,7 @@ angular.module('myApp.record', ['ngRoute'])
       y0: y0 / h,
       x1: x1 / w,
       y1: y1 / h,
+      lineWidth: lineWidth,
       color: color
     });
   }
@@ -164,28 +161,28 @@ angular.module('myApp.record', ['ngRoute'])
   function onMouseDown(e){
     drawing = true;
     var rect = canvas.getBoundingClientRect();
-    current.x = (e.clientX - rect.left) * scale;
-    current.y = (e.clientY - rect.top) * scale;
+    $scope.chalk.x = (e.clientX - rect.left) * scale;
+    $scope.chalk.y = (e.clientY - rect.top) * scale;
   }
 
   function onMouseUp(e){
     if (!drawing) { return; }
     drawing = false;
     var rect = canvas.getBoundingClientRect();
-    drawLine(current.x, current.y, (e.clientX - rect.left) * scale, (e.clientY - rect.top) * scale, current.color, true);
+    drawLine($scope.chalk.x, $scope.chalk.y, (e.clientX - rect.left) * scale, (e.clientY - rect.top) * scale, $scope.chalk.color, $scope.chalk.lineWidth, true);
     socket.emit('setLastCanvasState', exportCanvasState());
   }
 
   function onMouseMove(e){
     if (!drawing) { return; }
     var rect = canvas.getBoundingClientRect();
-    drawLine(current.x, current.y, (e.clientX - rect.left) * scale, (e.clientY - rect.top) * scale, current.color, true);
-    current.x = (e.clientX - rect.left) * scale;
-    current.y = (e.clientY - rect.top) * scale;
+    drawLine($scope.chalk.x, $scope.chalk.y, (e.clientX - rect.left) * scale, (e.clientY - rect.top) * scale, $scope.chalk.color, $scope.chalk.lineWidth, true);
+    $scope.chalk.x = (e.clientX - rect.left) * scale;
+    $scope.chalk.y = (e.clientY - rect.top) * scale;
   }
 
   function onColorUpdate(e){
-    current.color = e.target.className.split(' ')[1];
+    $scope.chalk.color = e.target.className.split(' ')[1];
   }
 
   // limit the number of events per second
@@ -204,7 +201,7 @@ angular.module('myApp.record', ['ngRoute'])
   function onDrawingEvent(data){
     var w = canvas.width;
     var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color, data.lineWidth);
   }
 
   // make the canvas fill its parent
